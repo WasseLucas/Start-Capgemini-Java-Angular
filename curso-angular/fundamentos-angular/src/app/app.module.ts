33a3389ae999a4febee90558c73c3b886bee50d0
Wasse Lucas <wasselucas.dev@gmail.com>
2023-07-14 10:46:53 -0300

    multiplica por

--- curso-angular/fundamentos-angular/src/app/app.module.ts
+++ curso-angular/fundamentos-angular/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeiroComponentComponent } from './components/primeiro-component/primeiro-component.component';
+import { SegundoComponentComponent } from './components/segundo-component/segundo-component.component';
+import { MultiplicaPorPipe } from '../app/utils/multiplica-por.pipe';
+import { PipeCustimizadoComponent } from './components/pipe-custimizado/pipe-custimizado.component';
+import { TwoWayDataBindingComponent } from './components/two-way-data-binding/two-way-data-binding.component';
+import { FormsModule } from '@angular/forms';
+import { RenderizandoListasComponent } from './components/renderizando-listas/renderizando-listas.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponentComponent,
+    SegundoComponentComponent,
+    MultiplicaPorPipe,
+    PipeCustimizadoComponent,
+    TwoWayDataBindingComponent,
+    RenderizandoListasComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
