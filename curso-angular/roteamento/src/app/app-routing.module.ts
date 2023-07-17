376cb69c13c0c14eb27406c07e0d8abd634a22f0
Wasse Lucas <wasselucas.dev@gmail.com>
2023-07-17 13:22:28 -0300

    roteamento / redirecionando pagina nao encontrada

--- curso-angular/roteamento/src/app/app-routing.module.ts
+++ curso-angular/roteamento/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router'
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 
 const routes: Routes = [
   {path:'primeira-pagina', component: PrimeiraPaginaComponent},//Renderizando primeira pagina{Rota}
   {path:'segunda-pagina', component: SegundaPaginaComponent}, //Renderizando segunda pagina {Rota}
-
+  {path:'', redirectTo: 'primeira-pagina', pathMatch: 'full'}, //Redirencionamendo da primeira pagina
+  {path:'**',component: PaginaNaoEncontradaComponent}
 ]
 
 @NgModule({
