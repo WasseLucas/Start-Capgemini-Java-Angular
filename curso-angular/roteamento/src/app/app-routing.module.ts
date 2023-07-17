af06ab9b2070af47144f7620e63a57f88e6961eb
Wasse Lucas <wasselucas.dev@gmail.com>
2023-07-17 13:10:15 -0300

    roteamento navegando entre paginas

--- curso-angular/roteamento/src/app/app-routing.module.ts
+++ curso-angular/roteamento/src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router'
+import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
+import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 
 
+const routes: Routes = [
+  {path:'primeira-pagina', component: PrimeiraPaginaComponent},//Renderizando primeira pagina{Rota}
+  {path:'segunda-pagina', component: SegundaPaginaComponent}, //Renderizando segunda pagina {Rota}
+
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+
 })
 export class AppRoutingModule { }
---
+++ curso-angular/roteamento/src/app/primeira-pagina/primeira-pagina.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-primeira-pagina',
+  templateUrl: './primeira-pagina.component.html',
+  styleUrls: ['./primeira-pagina.component.css']
+})
+export class PrimeiraPaginaComponent {
+
+  constructor(
+    private router:Router
+  ){}
+
+
+  //Methodo navegar para segunda página
+  moverParaSegundaPagina(){
+    this.router.navigate([
+      'segunda-pagina'
+    ])
+  }
+
+}
