68e2791f8fcc36dca4c10535d5c75ad8e2b9a038
Wasse Lucas <wasselucas.dev@gmail.com>
2023-07-17 15:50:58 -0300

    lazy-loading

--- curso-angular/roteamento/src/app/app-routing.module.ts
+++ curso-angular/roteamento/src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import {RouterModule, Routes} from '@angular/router'
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { PaginaComParamentrosComponent } from './pagina-com-paramentros/pagina-com-paramentros.component';
 
 
 const routes: Routes = [
   {path:'primeira-pagina', component: PrimeiraPaginaComponent},//Renderizando primeira pagina{Rota}
   {path:'segunda-pagina', component: SegundaPaginaComponent}, //Renderizando segunda pagina {Rota}
   {path:'', redirectTo: 'primeira-pagina', pathMatch: 'full'}, //Redirencionamendo da primeira pagina
-  {path:'**',component: PaginaNaoEncontradaComponent}
+  {path:'pagina-com-parametros/:id',component:PaginaComParamentrosComponent},//Redirecionando pagina com parametro id
+  {path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) },
+  {path:'**',component: PaginaNaoEncontradaComponent},//Redirencionando page not found
+  
 ]
 
 @NgModule({
