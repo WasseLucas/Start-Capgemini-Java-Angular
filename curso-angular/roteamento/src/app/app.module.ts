13d2a2591a193589b417aea2b05593f3f3ff4ed0
Wasse Lucas <wasselucas.dev@gmail.com>
2023-07-17 15:50:40 -0300

    redirecionamento de paginas

--- curso-angular/roteamento/src/app/app.module.ts
+++ curso-angular/roteamento/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { PaginaComParamentrosComponent } from './pagina-com-paramentros/pagina-com-paramentros.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimeiraPaginaComponent,
     SegundaPaginaComponent,
-    PaginaNaoEncontradaComponent
+    PaginaNaoEncontradaComponent,
+    PaginaComParamentrosComponent
   ],
   imports: [
     BrowserModule,
